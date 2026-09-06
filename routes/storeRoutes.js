5500f673c2269abd547a69569709ca0e086a36c0
mayankyadav19 <hatmario06@gmail.com>
2026-09-06 22:17:29 +0530

    implement post /favourite route

--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -3,11 +3,13 @@ const path = require('path');
 const express = require("express");
 const storeRouter = express.Router();
 
-const {getStoreHomes, getFavList, getReserve, getBookings, getHomeDetails} = require('../controllers/storeController');
+const {getStoreHomes, getFavList, postFavHome, getReserve, getBookings, getHomeDetails} = require('../controllers/storeController');
 
 storeRouter.get('/view-homes', getStoreHomes);
 
 storeRouter.get('/favourite', getFavList);
+
+storeRouter.post('/favourite', postFavHome);
 
 storeRouter.get('/reserve', getReserve);
 
