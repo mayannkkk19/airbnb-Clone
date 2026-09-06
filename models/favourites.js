124f8adb189f8046a6f51ab1bf617eafbe90b1fc
mayankyadav19 <hatmario06@gmail.com>
2026-09-06 22:20:29 +0530

    create model for fav home

---
+++ models/favourites.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = require("../utils/pathUtil");
+
+const favDataPath = path.join(rootDir, "data", "favourites.json");
+
+module.exports = class Favourite {
+
+  static getFavList(callback) {
+    fs.readFile(favDataPath, (err, data) => {
+      if (!err) {
+        callback(JSON.parse(data));
+      } else {
+        callback([]);
+      }
+    });
+  }
+
+  static addFavHome(homeID, callback) {
+    Favourite.getFavList((favourites) => {
+      favourites.push(homeID);
+      fs.writeFile(favDataPath, JSON.stringify(favourites), (error) => {
+        console.log("File writing concluded: ", error);
+        callback();
+      });
+    });
+  }
+};
